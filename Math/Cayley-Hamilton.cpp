ab11a75c3f0ab4d78d20dae7eab1eaf3865c1983
9646516 <38785664+9646516@users.noreply.github.com>
2020-05-24 16:23:49 +0800

    Update Cayley-Hamilton.cpp

--- Math/Cayley-Hamilton.cpp
+++ Math/Cayley-Hamilton.cpp
@@ -1,69 +1,43 @@
-//BZOJ4161
-#include<bits/stdc++.h>
-using namespace std;
-const int MAXN = 4005;
-const int P = 1e9 + 7;
-template <typename T> void chkmax(T &x, T y) {x = max(x, y); }
-template <typename T> void chkmin(T &x, T y) {x = min(x, y); } 
-template <typename T> void read(T &x) {
-	x = 0; int f = 1;
-	char c = getchar();
-	for (; !isdigit(c); c = getchar()) if (c == '-') f = -f;
-	for (; isdigit(c); c = getchar()) x = x * 10 + c - '0';
-	x *= f;
-}
-template <typename T> void write(T x) {
-	if (x < 0) x = -x, putchar('-');
-	if (x > 9) write(x / 10);
-	putchar(x % 10 + '0');
-}
-template <typename T> void writeln(T x) {
-	write(x);
-	puts("");
-}
-int n, k;
-int a[MAXN], now[MAXN];
-int res[MAXN], h[MAXN];
-void times(int *x, int *y) {
-	static int tmp[MAXN];
-	memset(tmp, 0, sizeof(tmp));
-	for (int i = 0; i <= k - 1; i++)
-	for (int j = 0; j <= k - 1; j++)
-		tmp[i + j] = (tmp[i + j] + 1ll * x[i] * y[j]) % P;
-	for (int i = 2 * k - 2; i >= k; i--) {
-		for (int j = 1; j <= k; j++)
-			tmp[i - j] = (tmp[i - j] + 1ll * tmp[i] * a[j]) % P;
-		tmp[i] = 0;
-	}
-	for (int i = 0; i <= k - 1; i++)
-		x[i] = tmp[i];
-}
-int main() {
-	read(n), read(k);
-	for (int i = 1; i <= k; i++) {
-		read(a[i]);
-		if (a[i] < 0) a[i] += P;
-	}
-	for (int i = 0; i <= k - 1; i++) {
-		read(h[i]);
-		if (h[i] < 0) h[i] += P;
-	}
-	if (n <= k - 1) {
-		writeln(h[k]);
-		return 0;
-	}
-	n -= k - 1; res[0] = now[1] = 1;
-	while (n != 0) {
-		if (n & 1) times(res, now);
-		n >>= 1;
-		times(now, now);
-	}
-	for (int i = k; i <= 2 * k - 2; i++)
-	for (int j = 1; j <= k; j++)
-		h[i] = (h[i] + 1ll * h[i - j] * a[j]) % P;
-	int ans = 0;
-	for (int i = 0; i <= k - 1; i++)
-		ans = (ans + 1ll * h[k - 1 + i] * res[i]) % P;
-	writeln(ans);
-	return 0;
+//https://ac.nowcoder.com/acm/contest/view-submission?submissionId=40900331
+void solve(ll n,int m,int a[],int c[],int p){
+    ll v[M]={1%p},u[M<<1],mask=!!n;
+    for(long long i(n);i>1;i>>=1){
+        mask<<=1;
+    }
+    for(long long x(0);mask;mask>>=1,x<<=1){
+        fill_n(u,m<<1,0);
+        int b(!!(n&mask));
+        x|=b;
+        if(x<m){
+            u[x]=1%MOD;
+        }else{
+            for(int i(0);i<m;i++){
+                for(int j(0),t(i+b);j<m;j++,t++){
+                    u[t]=(u[t]+v[i]*v[j])%MOD;
+                }
+            }
+            for(int i((m<<1)-1);i>=m;i--){
+                for(int j(0),t(i-m);j<m;j++,t++){
+                    u[t]=(u[t]+c[j]*u[i])%MOD;
+                }
+            }
+        }
+        copy(u,u+m,v);
+    }
+    for(int i(m);i<2*m;i++){
+        a[i]=0;
+        for(int j(0);j<m;j++){
+            a[i]=(a[i]+(ll)c[j]*a[i+j-m])%MOD;
+        }
+    }
+    ll b[M];
+    for(int j(0);j<m;j++){
+        b[j]=0;
+        for(int i(0);i<m;i++){
+            b[j]=(b[j]+v[i]*a[i+j])%MOD;
+        }
+    }
+    for(int j(0);j<m;j++){
+        a[j]=b[j];
+    }
 }
