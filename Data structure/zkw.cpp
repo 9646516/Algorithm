96db2da17a6d842053901cc79aafbd1e5782e767
9646516 <38785664+9646516@users.noreply.github.com>
2020-06-20 19:10:41 +0800

    Update zkw.cpp

--- Data structure/zkw.cpp
+++ Data structure/zkw.cpp
@@ -1,15 +1,14 @@
-using ll = long long;
-using T = pair<ll, ll>;
+using T = int;
 const int N = 1 << 22;
 #define F0R(i, a) for (int i = 0; i < (a); i++)
 #define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
 template <int SZ> struct Seg {
-    T seg[2 * SZ], ID = make_pair(LONG_LONG_MIN, LONG_LONG_MAX);
-    Seg() { F0R(i, 2 * SZ) seg[i] = ID; }
-    T comb(const T &a, const T &b) const {
-        return make_pair(max(a.first, b.first), min(a.second, b.second));
-    }
+    T seg[2 * SZ];
+    static const ID = 0;
+    void init() { F0R(i, 2 * SZ) seg[i] = ID; }
+    Seg() { init(); }
     void build() { F0Rd(i, SZ) seg[i] = comb(seg[2 * i], seg[2 * i + 1]); }
+    T comb(const T &a, const T &b) const { return a + b; }
     void upd(int p, T value) {
         for (seg[p += SZ] += value; p > 1; p >>= 1)
             seg[p >> 1] = comb(seg[(p | 1) ^ 1], seg[p | 1]);
