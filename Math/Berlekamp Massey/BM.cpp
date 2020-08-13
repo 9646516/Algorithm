9cc35b7523d62b5225cfd0ba8fc01e187c46a7ff
9646516 <38785664+9646516@users.noreply.github.com>
2020-08-13 23:43:20 +0800

    Update BM.cpp

--- Math/Berlekamp Massey/BM.cpp
+++ Math/Berlekamp Massey/BM.cpp
@@ -1,109 +1,90 @@
-struct Massey {
-    static const int mod = MOD;
-    void add_self(int &a, int b) {
-        a += b;
-        if (a >= mod)
-            a -= mod;
-    }
-    void sub_self(int &a, int b) {
-        a -= b;
-        if (a < 0)
-            a += mod;
-    }
-    int mul(int a, int b) { return (ll)a * b % mod; }
-    int my_pow(int a, int b) {
-        int r = 1;
-        while (b) {
-            if (b % 2)
-                r = mul(r, a);
-            a = mul(a, a);
-            b /= 2;
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int MOD = 1e9 + 7;
+namespace linear_seq {
+#define rep(i, a, n) for (long long i = a; i < n; i++)
+#define SZ(x) ((long long)(x).size())
+    const long long N = 10010;
+    ll xpow(ll a, ll b) {
+        ll res = 1;
+        a %= MOD;
+        assert(b >= 0);
+        for (; b; b >>= 1) {
+            if (b & 1)
+                res = res * a % MOD;
+            a = a * a % MOD;
         }
-        return r;
+        return res;
     }
-    int my_inv(int a) { return my_pow(a, mod - 2); }
-
-    vector<int> start, coef; // 3 optional lines
-    vector<vector<int>> powers;
-    int memo_inv;
-
-    // Start here and write the next ~25 lines until "STOP"
-
-    int L;                   // L == coef.size() <= start.size()
-    Massey(vector<int> in) { // O(N^2)
-        L = 0;
-        const int N = in.size();
-        vector<int> C{1}, B{1};
-        for (int n = 0; n < N; ++n) {
-            assert(0 <= in[n] && in[n] < mod); // invalid input
-            B.insert(B.begin(), 0);
-            int d = 0;
-            for (int i = 0; i <= L; ++i)
-                add_self(d, mul(C[i], in[n - i]));
-            if (d == 0)
-                continue;
-            vector<int> T = C;
-            C.resize(max(B.size(), C.size()));
-            for (int i = 0; i < (int)B.size(); ++i)
-                sub_self(C[i], mul(d, B[i]));
-            if (2 * L <= n) {
-                L = n + 1 - L;
-                B = T;
-                d = my_inv(d);
-                for (int &x : B)
-                    x = mul(x, d);
+    ll res[N], base[N], _c[N], _md[N];
+    vector<long long> Md;
+    void mul(ll *a, ll *b, long long k) {
+        rep(i, 0, k + k) _c[i] = 0;
+        rep(i, 0, k) if (a[i]) rep(j, 0, k) _c[i + j] = (_c[i + j] + a[i] * b[j]) % MOD;
+        for (long long i = k + k - 1; i >= k; i--)
+            if (_c[i])
+                rep(j, 0, SZ(Md)) _c[i - k + Md[j]] = (_c[i - k + Md[j]] - _c[i] * _md[Md[j]]) % MOD;
+        rep(i, 0, k) a[i] = _c[i];
+    }
+    long long solve(ll n, vector<long long> a, vector<long long> b) {
+        ll ans = 0, pnt = 0;
+        long long k = SZ(a);
+        assert(SZ(a) == SZ(b));
+        rep(i, 0, k) _md[k - 1 - i] = -a[i];
+        _md[k] = 1;
+        Md.clear();
+        rep(i, 0, k) if (_md[i] != 0) Md.push_back(i);
+        rep(i, 0, k) res[i] = base[i] = 0;
+        res[0] = 1;
+        while ((1ll << pnt) <= n)
+            pnt++;
+        for (long long p = pnt; p >= 0; p--) {
+            mul(res, res, k);
+            if ((n >> p) & 1) {
+                for (long long i = k - 1; i >= 0; i--)
+                    res[i + 1] = res[i];
+                res[0] = 0;
+                rep(j, 0, SZ(Md)) res[Md[j]] = (res[Md[j]] - res[k] * _md[Md[j]]) % MOD;
             }
         }
-        // cerr << "L = " << L << "\n";
-        assert(2 * L <= N - 2); // NO RELATION FOUND :(
-        // === STOP ===
-        for (int i = 1; i < (int)C.size(); ++i)
-            coef.push_back((mod - C[i]) % mod);
-        assert((int)coef.size() == L);
-        for (int i = 0; i < L; ++i)
-            start.push_back(in[i]);
-        while (!coef.empty() && !coef.back()) {
-            coef.pop_back();
-            --L;
-        }
-        if (!coef.empty())
-            memo_inv = my_inv(coef.back());
-        powers.push_back(coef);
-        //~ debug() << imie(coef);
+        rep(i, 0, k) ans = (ans + res[i] * b[i]) % MOD;
+        if (ans < 0)
+            ans += MOD;
+        return ans;
     }
-
-    vector<int> mul_cut(vector<int> a, vector<int> b) {
-        vector<int> r(2 * L - 1);
-        for (int i = 0; i < L; ++i)
-            for (int j = 0; j < L; ++j)
-                add_self(r[i + j], mul(a[i], b[j]));
-        while ((int)r.size() > L) {
-            int value = mul(r.back(), memo_inv); // div(r.back(), coef.back());
-            const int X = r.size();
-            add_self(r[X - L - 1], value);
-            for (int i = 0; i < L; ++i)
-                sub_self(r[X - L + i], mul(value, coef[i]));
-            assert(r.back() == 0);
-            r.pop_back();
+    vector<long long> BM(vector<long long> s) {
+        vector<long long> C(1, 1), B(1, 1);
+        long long L = 0, m = 1, b = 1;
+        rep(n, 0, SZ(s)) {
+            ll d = 0;
+            rep(i, 0, L + 1) d = (d + (ll)C[i] * s[n - i]) % MOD;
+            if (d == 0)
+                ++m;
+            else if (2 * L <= n) {
+                vector<long long> T = C;
+                ll c = MOD - d * xpow(b, MOD - 2) % MOD;
+                while (SZ(C) < SZ(B) + m)
+                    C.push_back(0);
+                rep(i, 0, SZ(B)) C[i + m] = (C[i + m] + c * B[i]) % MOD;
+                L = n + 1 - L;
+                B = T;
+                b = d;
+                m = 1;
+            } else {
+                ll c = MOD - d * xpow(b, MOD - 2) % MOD;
+                while (SZ(C) < SZ(B) + m)
+                    C.push_back(0);
+                rep(i, 0, SZ(B)) C[i + m] = (C[i + m] + c * B[i]) % MOD;
+                ++m;
+            }
         }
-        return r;
+        return C;
     }
-    int get(ll k) { // O(L^2 * log(k))
-        if (k < (int)start.size())
-            return start[k];
-        if (L == 0)
-            return 0;
-        k -= start.size();
-        vector<int> vec = coef;
-        for (int i = 0; (1LL << i) <= k; ++i) {
-            if (i == (int)powers.size())
-                powers.push_back(mul_cut(powers.back(), powers.back()));
-            if (k & (1LL << i))
-                vec = mul_cut(vec, powers[i]);
-        }
-        int total = 0;
-        for (int i = 0; i < L; ++i)
-            add_self(total, mul(vec[i], start[(int)start.size() - 1 - i]));
-        return total;
+    long long gao(vector<long long> a, ll n) {
+        vector<long long> c = BM(a);
+        c.erase(c.begin());
+        rep(i, 0, SZ(c)) c[i] = (MOD - c[i]) % MOD;
+        return solve(n, c, vector<long long>(a.begin(), a.begin() + SZ(c)));
     }
-};
+}; // namespace linear_seq
