d5faf06a3674ab7ab9b11555213a61af33690d6b
9646516 <38785664+9646516@users.noreply.github.com>
2020-08-13 23:15:46 +0800

    Update Cayley-Hamilton.cpp

--- Math/Cayley-Hamilton.cpp
+++ Math/Cayley-Hamilton.cpp
@@ -1,43 +1,49 @@
-//https://ac.nowcoder.com/acm/contest/view-submission?submissionId=40900331
-void solve(ll n,int m,int a[],int c[],int p){
-    ll v[M]={1%p},u[M<<1],mask=!!n;
-    for(long long i(n);i>1;i>>=1){
-        mask<<=1;
+using ll = long long;
+const int MOD = 1e9 + 7;
+// Kth,矩阵大小,初始值,转移方程
+// a F0,F1 =>Fn,Fn+1
+void solve(ll n, int m, int a[], int c[]) {
+    if (!n)
+        return;
+    vector<ll> v(m, 1 % MOD), u(m << 1);
+    ll mask = !!n;
+    for (ll i = n; i > 1; i >>= 1) {
+        mask <<= 1;
     }
-    for(long long x(0);mask;mask>>=1,x<<=1){
-        fill_n(u,m<<1,0);
-        int b(!!(n&mask));
-        x|=b;
-        if(x<m){
-            u[x]=1%MOD;
-        }else{
-            for(int i(0);i<m;i++){
-                for(int j(0),t(i+b);j<m;j++,t++){
-                    u[t]=(u[t]+v[i]*v[j])%MOD;
+    for (ll x = 0; mask; mask >>= 1, x <<= 1) {
+        fill_n(u.begin(), m << 1, 0);
+        int b = !!(n & mask);
+        x |= b;
+        if (x < m) {
+            u[x] = 1 % MOD;
+        } else {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0, t = i + b; j < m; j++, t++) {
+                    u[t] = (u[t] + v[i] * v[j]) % MOD;
                 }
             }
-            for(int i((m<<1)-1);i>=m;i--){
-                for(int j(0),t(i-m);j<m;j++,t++){
-                    u[t]=(u[t]+c[j]*u[i])%MOD;
+            for (int i = (m << 1) - 1; i >= m; i--) {
+                for (int j = 0, t = i - m; j < m; j++, t++) {
+                    u[t] = (u[t] + c[j] * u[i]) % MOD;
                 }
             }
         }
-        copy(u,u+m,v);
+        copy(u.begin(), u.begin() + m, v.begin());
     }
-    for(int i(m);i<2*m;i++){
-        a[i]=0;
-        for(int j(0);j<m;j++){
-            a[i]=(a[i]+(ll)c[j]*a[i+j-m])%MOD;
+    for (int i = m; i < 2 * m; i++) {
+        a[i] = 0;
+        for (int j = 0; j < m; j++) {
+            a[i] = (a[i] + (ll)c[j] * a[i + j - m]) % MOD;
         }
     }
-    ll b[M];
-    for(int j(0);j<m;j++){
-        b[j]=0;
-        for(int i(0);i<m;i++){
-            b[j]=(b[j]+v[i]*a[i+j])%MOD;
+    vector<ll> b(m);
+    for (int j = 0; j < m; j++) {
+        b[j] = 0;
+        for (int i = 0; i < m; i++) {
+            b[j] = (b[j] + v[i] * a[i + j]) % MOD;
         }
     }
-    for(int j(0);j<m;j++){
-        a[j]=b[j];
+    for (int j = 0; j < m; j++) {
+        a[j] = b[j];
     }
 }
