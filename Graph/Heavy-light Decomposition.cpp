b037b923d96ccadeae8c70481b154906e6a0b3a8
9646516 <38785664+9646516@users.noreply.github.com>
2020-08-15 22:52:18 +0800

    Update Heavy-light Decomposition.cpp

--- Graph/Heavy-light Decomposition.cpp
+++ Graph/Heavy-light Decomposition.cpp
@@ -1,53 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-template <typename base_type, base_type MOD> struct IntMod {
-    base_type n;
-    IntMod(long long d = 0) { n = (d >= 0 ? d % MOD : (d % MOD + MOD) % MOD); }
-    IntMod operator-() const { return build(n == 0 ? 0 : MOD - n); }
-    IntMod &operator+=(IntMod a) {
-        n = (n >= MOD - a.n ? n - MOD + a.n : n + a.n);
-        return *this;
-    }
-    IntMod &operator-=(IntMod a) {
-        n = (n >= a.n) ? n - a.n : n - a.n + MOD;
-        return *this;
-    }
-    IntMod &operator*=(IntMod a) {
-        *this = *this * a;
-        return *this;
-    }
-    static IntMod build(base_type n) {
-        IntMod r;
-        r.n = n;
-        return r;
-    }
-    friend bool operator==(IntMod a, IntMod b) { return a.n == b.n; }
-    friend bool operator!=(IntMod a, IntMod b) { return a.n != b.n; }
-    friend IntMod operator+(IntMod a, IntMod b) { return build(a.n >= MOD - b.n ? a.n - MOD + b.n : a.n + b.n); }
-    friend IntMod operator-(IntMod a, IntMod b) { return build(a.n >= b.n ? a.n - b.n : a.n - b.n + MOD); }
-    friend IntMod operator*(IntMod a, IntMod b) { return build(static_cast<base_type>(static_cast<long long>(a.n) * b.n % MOD)); }
-    friend istream &operator>>(istream &stream, IntMod &a) {
-        stream >> a.n;
-        return stream;
-    }
-    friend ostream &operator<<(ostream &stream, const IntMod &a) {
-        stream << a.n;
-        return stream;
-    }
-};
-const long long MOD = 1e9 + 7;
-using Int = IntMod<int, MOD>;
+template <int SZ> struct Seg {
 #define F0R(i, a) for (int i = 0; i < (a); i++)
 #define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
-using T = Int;
-const T ID = 1;
-struct HLD {
-    static const int maxn = 1e5 + 555;
-    vector<int> V[maxn];
-    int fa[maxn],dep[maxn],sum[maxn], son[maxn];
-    int idx[maxn]; // 下标转dfs序
-    int raw[maxn]; // dfs序转下标
-    int top[maxn]; // i点所在链的顶端
+    using T = int;
+    static const T ID = 0;
+    T seg[2 * SZ];
+    void init() { F0R(i, 2 * SZ) seg[i] = ID; }
+    Seg() { init(); }
+    void set(const vector<T> &x) {
+        assert((int)x.size() <= SZ);
+        for (int i = 0; i < (int)x.size(); i++) {
+            seg[i + SZ] = x[i];
+        }
+        build();
+    }
+    void set(const T &x) {
+        for (int i = 0; i < SZ; i++) {
+            seg[i + SZ] = x;
+        }
+        build();
+    }
+    void build() { F0Rd(i, SZ) seg[i] = comb(seg[2 * i], seg[2 * i + 1]); }
+    T comb(const T &a, const T &b) const { return a + b; }
+    void upd(int p, T value) {
+        for (seg[p += SZ] = value; p > 1; p >>= 1)
+            seg[p >> 1] = comb(seg[(p | 1) ^ 1], seg[p | 1]);
+    }
+    T query(int l, int r) {
+        if (l > r)
+            return 0;
+        T res1 = ID, res2 = ID;
+        r++;
+        for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
+            if (l & 1)
+                res1 = comb(res1, seg[l++]);
+            if (r & 1)
+                res2 = comb(seg[--r], res2);
+        }
+        return comb(res1, res2);
+    }
+};
+template <int N> struct HLD : public Seg<N> {
+    vector<int> V[N];
+    int fa[N], dep[N], sum[N], son[N];
+    int idx[N]; // 下标转dfs序
+    int raw[N]; // dfs序转下标
+    int top[N]; // i点所在链的顶端
     void dfs1(int now, int pre, int d) {
         fa[now] = pre;
         dep[now] = d;
@@ -75,55 +72,41 @@ struct HLD {
             dfs2(i, now, i, dfn);
         }
     }
-    static constexpr int N = 1 << (__lg(maxn) + 1);
-    template <int SZ> struct Seg {
-        T seg[2 * SZ];
-        void init() { F0R(i, 2 * SZ) seg[i] = ID; }
-        Seg() { init(); }
-        void set(const vector<T> &x) {
-            assert((int)x.size() <= SZ);
-            for (int i = 0; i < (int)x.size(); i++) {
-                seg[i + SZ] = x[i];
-            }
-            build();
-        }
-        void build() { F0Rd(i, SZ) seg[i] = comb(seg[2 * i], seg[2 * i + 1]); }
-        T comb(const T &a, const T &b) const { return a * b; }
-        void upd(int p, T value) {
-            for (seg[p += SZ] = value; p > 1; p >>= 1)
-                seg[p >> 1] = comb(seg[(p | 1) ^ 1], seg[p | 1]);
-        }
-        T query(int l, int r) {
-            if (l > r)
-                return 0;
-            T res1 = ID, res2 = ID;
-            r++;
-            for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
-                if (l & 1)
-                    res1 = comb(res1, seg[l++]);
-                if (r & 1)
-                    res2 = comb(seg[--r], res2);
-            }
-            return comb(res1, res2);
-        }
-    };
-    Seg<N> seg;
-    int query(int x, int y) {
-        Int ret = 1;
+    int lca(int x, int y) {
         int fa1 = top[x], fa2 = top[y];
         while (fa1 != fa2) {
             if (dep[fa1] < dep[fa2]) {
                 swap(fa1, fa2);
                 swap(x, y);
             }
-            ret *= seg.query(idx[fa1], idx[x]);
+            x = fa[fa1];
+            fa1 = top[x];
+        }
+        if (dep[x] > dep[y])
+            return y;
+        else
+            return x;
+    }
+    int distance(int x, int y) {
+        int LCA = lca(x, y);
+        return dep[x] + dep[y] - dep[LCA] - dep[LCA];
+    }
+    int query(int x, int y) {
+        int ret = 0;
+        int fa1 = top[x], fa2 = top[y];
+        while (fa1 != fa2) {
+            if (dep[fa1] < dep[fa2]) {
+                swap(fa1, fa2);
+                swap(x, y);
+            }
+            ret += query(idx[fa1], idx[x]);
             x = fa[fa1];
             fa1 = top[x];
         }
         if (idx[x] > idx[y])
             swap(x, y);
-        ret *= seg.query(idx[x], idx[y]);
-        return ret.n;
+        ret += query(idx[x], idx[y]);
+        return ret;
     }
     void add(int u, int v) {
         V[u].push_back(v);
@@ -135,25 +118,6 @@ struct HLD {
         dfs1(root, -1, 0);
         dfs2(root, -1, root, idx);
     }
-} HLD;
-
-class Solution {
-  public:
-    vector<int> solve(int n, int m, vector<int> &a, vector<int> &u, vector<int> &v, vector<int> &x, vector<int> &y) {
-        for (int i = 0; i < n - 1; i++) {
-            int x = u[i], y = v[i];
-            HLD.add(x, y);
-        }
-        HLD.init();
-        vector<Int> val;
-        for (int i = 0; i < n; i++)
-            val.push_back(a[HLD.raw[i] - 1]);
-        HLD.seg.set(val);
-        vector<int> ret;
-        for (int i = 0; i < m; i++) {
-            int u = x[i], v = y[i];
-            ret.push_back(HLD.query(u, v));
-        }
-        return ret;
-    }
 };
+const int maxn = 2e5 + 555;
+constexpr int N = 1 << (__lg(maxn) + 1);
