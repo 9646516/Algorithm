696165ab4e1a476049b9d7bfec5277564366c466
9646516 <38785664+9646516@users.noreply.github.com>
2020-08-15 22:52:41 +0800

    Update zkw.cpp

--- Data structure/zkw.cpp
+++ Data structure/zkw.cpp
@@ -1,12 +1,24 @@
-using T = int;
-const int N = 1 << 22;
+template <int SZ> struct Seg {
 #define F0R(i, a) for (int i = 0; i < (a); i++)
 #define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
-template <int SZ> struct Seg {
-    T seg[2 * SZ];
+    using T = int;
     static const T ID = 0;
+    T seg[2 * SZ];
     void init() { F0R(i, 2 * SZ) seg[i] = ID; }
     Seg() { init(); }
+    void set(const vector<T> &x) {
+        assert((int)x.size() <= SZ);
+        for (int i = 0; i < (int)x.size(); i++) {
+            seg[i + SZ] = x[i];
+        }
+        build();
+    }
+    void set(const T &x) {
+        for (int i = 0; i < SZ; i++) {
+            seg[i + SZ] = x;
+        }
+        build();
+    }
     void build() { F0Rd(i, SZ) seg[i] = comb(seg[2 * i], seg[2 * i + 1]); }
     T comb(const T &a, const T &b) const { return a + b; }
     void upd(int p, T value) {
@@ -14,6 +26,8 @@ template <int SZ> struct Seg {
             seg[p >> 1] = comb(seg[(p | 1) ^ 1], seg[p | 1]);
     }
     T query(int l, int r) {
+        if (l > r)
+            return 0;
         T res1 = ID, res2 = ID;
         r++;
         for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
